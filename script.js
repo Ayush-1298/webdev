4e8d68663290fa918aa269d1918476047aae0801
Ayush-1298 <55658734+Ayush-1298@users.noreply.github.com>
2020-06-28 22:09:12 +0530

    Module4Solution

---
+++ script.js
@@ -0,0 +1,11 @@
+(function() {
+    var names = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
